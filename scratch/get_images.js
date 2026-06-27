f9c94954b1f44d3580a8b069a3d80d6cb3f91b21
Enis Sahide <enissahide.kesik@outlook.com>
2026-06-27 12:21:45 +0300

    run get_images page 48 script

--- scratch/get_images.js
+++ scratch/get_images.js
@@ -1,6 +1,6 @@
 const https = require('https');
 
-https.get('https://sos70.ru/', (res) => {
+https.get('https://sos70.ru/?page_id=48', (res) => {
   let data = '';
   res.on('data', (chunk) => {
     data += chunk;
@@ -8,7 +8,7 @@ https.get('https://sos70.ru/', (res) => {
   res.on('end', () => {
     const regex = /<img[^>]+src="([^">]+)"/g;
     let match;
-    console.log("Found images:");
+    console.log("Found images on page 48:");
     while ((match = regex.exec(data)) !== null) {
       console.log(match[1]);
     }
